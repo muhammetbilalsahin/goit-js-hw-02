3badad3b0da1290edb06e3f55b330c5c3599beff
sahinbilisim <shnblsm@gmail.com>
2025-02-20 14:58:21 +0300

    js-2

--- js/task-4.js
+++ js/task-4.js
@@ -1,14 +1,22 @@
 function getShippingCost(country) {
-  const deliveryCountries = {
-    China: 100,
-    Chile: 250,
-    Australia: 170,
-    Jamaica: 120,
-  };
-  if (deliveryCountries[country]) {
-    return `Shipping to ${country} will cost ${deliveryCountries[country]} credits`;
+  let price;
+  switch (country) {
+    case 'China':
+      price = 100;
+      break;
+    case 'Chile':
+      price = 250;
+      break;
+    case 'Australia':
+      price = 170;
+      break;
+    case 'Jamaica':
+      price = 120;
+      break;
+    default:
+      return 'Sorry, there is no delivery to your country';
   }
-  return 'Sorry, there is no delivery to your country';
+  return `Shipping to ${country} will cost ${price} credits`;
 }
 console.log(getShippingCost('Australia'));
 console.log(getShippingCost('Germany'));
